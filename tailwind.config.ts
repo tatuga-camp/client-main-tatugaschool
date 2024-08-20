12eeb3f204180b3f9b741a9e047c8a1cb1191a1a
permlap-phola <permlap@tatugacamp.com>
2024-08-20 15:00:33 +0700

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Anuphan: ["Anuphan", "sans-serif"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "primary-color": "#6149CD",
+        "secondary-color": "#569DF8",
+        "info-color": "#2E90FA",
+        "success-color": "#27AE60",
+        "warning-color": "#FFCD1B",
+        "error-color": "#F04438",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
