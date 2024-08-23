6c35b1e76658cdb827ae9b10fdc244a456a8114b
permlap-phola <permlap@tatugacamp.com>
2024-08-23 15:37:15 +0700

    Change file user.ts to User.ts on Interface (#3)

---
+++ interfaces/User.ts
@@ -0,0 +1,26 @@
+export type User = {
+  id: string;
+  createAt: Date;
+  updateAt: Date;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  photo: string;
+  password?: string;
+  role: UserRole;
+  createBySchoolId?: string;
+  isVerifyEmail: boolean;
+  verifyEmailToken?: string;
+  verifyEmailTokenExpiresAt?: Date;
+  lastActiveAt: Date;
+  isResetPassword: boolean;
+  provider: Provider;
+  isDeleted: boolean;
+  deleteAt?: Date;
+  resetPasswordToken?: string;
+  resetPasswordTokenExpiresAt?: Date;
+};
+
+export type UserRole = "ADMIN" | "USER";
+export type Provider = "LOCAL" | "GOOGLE";
