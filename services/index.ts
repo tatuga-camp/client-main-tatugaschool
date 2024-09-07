83334237e919b9ec2c44a2361b06611c15afb9d5
permlap-phola <permlap@tatugacamp.com>
2024-09-07 15:19:05 +0700

    Add Index service

--- services/index.ts
+++ services/index.ts
@@ -1,1 +1,6 @@
 export * from "./auth";
+export * from "./user";
+export * from "./assignments";
+export * from "./attendance";
+export * from "./attendance-row";
+export * from "./attendance-table";
