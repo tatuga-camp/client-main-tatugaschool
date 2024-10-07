64725f5cd39cab27d3917ed10cdb519a177ece8a
permlap-phola <permlap@tatugacamp.com>
2024-10-08 01:32:04 +0700

    Implement React-Query (#15)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from "next/app";
 import { Prompt } from "@next/font/google";
 import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
 import { PagesProgressBar } from "next-nprogress-bar";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const prompt = Prompt({
   subsets: ["latin", "thai"],
@@ -10,18 +13,33 @@ const prompt = Prompt({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 60, // 1 hour in ms
+            refetchOnMount: false, // Disables automatic refetching when component is mounted.removed
+            refetchOnWindowFocus: false, // Disables automatic refetching when browser window is focused.
+          },
+        },
+      })
+  );
   return (
-    <PrimeReactProvider>
-      <PagesProgressBar
-        height="4px"
-        color="#6149CD"
-        options={{ showSpinner: false }}
-        shallowRouting
-      />
-      <div className={prompt.className}>
-        <Component {...pageProps} />
-      </div>
-    </PrimeReactProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <PrimeReactProvider>
+        <PagesProgressBar
+          height="4px"
+          color="#6149CD"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+        <div className={prompt.className}>
+          <Component {...pageProps} />
+        </div>
+      </PrimeReactProvider>
+    </QueryClientProvider>
   );
 }
 
