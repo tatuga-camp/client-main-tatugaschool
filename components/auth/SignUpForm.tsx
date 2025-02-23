5b38b1cde0076e93cb6a4a8d9561acc926ffff4a
permlap-phola <permlap@tatugacamp.com>
2025-02-23 16:16:41 +0700

    Add Google Button to sign up page (#111)

--- components/auth/SignUpForm.tsx
+++ components/auth/SignUpForm.tsx
@@ -94,10 +94,35 @@ export const SignUpForm = (props: Props) => {
     }
   };
 
+  const handleGoogleLogin = async () => {
+    try {
+      Swal.fire({
+        title: "Please wait...",
+        text: "We are processing your request",
+        showConfirmButton: false,
+        willOpen: () => {
+          Swal.showLoading();
+        },
+      });
+      router.push(`${process.env.NEXT_PUBLIC_SERVER_URL}/v1/auth/google`);
+    } catch (error) {
+      console.log(error);
+      let result = error as ErrorMessages;
+      Swal.fire({
+        title: result?.error ? result?.error : "Something Went Wrong",
+        text: result?.message?.toString(),
+        footer: result?.statusCode
+          ? "Code Error: " + result?.statusCode?.toString()
+          : "",
+        icon: "error",
+      });
+    }
+  };
+
   return (
     <form
       className="bg-white p-10   rounded-lg  flex flex-col
-       gap-4 w-96 md:w-5/12  shadow-[0_12px_24px_rgba(145,158,171,0.12)] text-center"
+       gap-4 w-96 md:w-5/12 items-center  shadow-[0_12px_24px_rgba(145,158,171,0.12)] text-center"
       onSubmit={handleSignUp}
     >
       <h2 className="text-[24px] font-bold">Create Account</h2>
@@ -202,11 +227,21 @@ export const SignUpForm = (props: Props) => {
         disabled={!isAgree}
         className={`p-5 ${
           isAgree ? "bg-secondary-color hover:bg-primary-color" : "bg-gray-400"
-        }  text-white rounded h-5 flex items-center justify-center
+        }  text-white rounded w-80 h-5 flex items-center justify-center
             font-semibold  transition duration-300`}
       >
         Create Account
       </button>
+      {props.provider !== "google" && (
+        <button
+          type="button"
+          onClick={handleGoogleLogin}
+          className="second-button w-80 border gap-2 flex items-center justify-center"
+        >
+          <FcGoogle />
+          Create Account with Google
+        </button>
+      )}
     </form>
   );
 };
