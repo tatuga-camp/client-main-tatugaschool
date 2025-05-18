a177935e72d41d68e0150d092afc78da352013a5
Warodom Pomsuwan <warodom@orcsoft.co.th>
2025-05-18 23:40:11 +0700

    feat: [form feedback]  add Botton and view

--- components/layout/SchoolLayout.tsx
+++ components/layout/SchoolLayout.tsx
@@ -5,6 +5,9 @@ import useClickOutside from "../../hook/useClickOutside";
 import { menuSchoolList } from "../../data";
 import { useGetSchool, useGetUser } from "../../react-query";
 import TawkToChat from "../common/TawkToChat";
+import { FeedbackTag } from "@/services/feedback";
+import FormFeedback from "../feedback/FormFeedback";
+import { useCreateFeedback } from "@/react-query/feedback";
 
 type LayoutProps = {
   children: ReactNode;
@@ -23,12 +26,14 @@ function SchoolLayout({
   const [active, setActive] = React.useState(false);
   const school = useGetSchool({ schoolId });
   const user = useGetUser();
+  const createFeedback = useCreateFeedback();
+
   useClickOutside(navbarRef, () => {
     setActive(() => false); // Close the SubjectNavbar when clicking outside
   });
   return (
-    <section className="min-h-screen font-Anuphan bg-background-color ">
-      <div ref={navbarRef} className="sticky z-50 top-0">
+    <section className="min-h-screen bg-background-color font-Anuphan">
+      <div ref={navbarRef} className="sticky top-0 z-50">
         <Navbar
           menuLists={menuSchoolList({ schoolId: schoolId })}
           schoolId={schoolId}
@@ -39,6 +44,17 @@ function SchoolLayout({
         />
       </div>
       {children}
+      <FormFeedback
+        onSubmit={(feedback) => {
+          createFeedback.mutate({
+            title: "User Feedback",
+            body: feedback,
+            tag: FeedbackTag.COMPLIMENT,
+          });
+        }}
+        onClose={() => {}}
+        schoolId="123"
+      />
       {school.data && user.data && (
         <TawkToChat school={school.data} user={user.data} />
       )}
