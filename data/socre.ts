04cdc237e506f12a9d43946fdfeca8f01f6e9538
Permlap Phola <permlap@tatugacamp.com>
2026-06-12 11:42:23 +0700

    fix bug prefix url is incorrect

--- data/socre.ts
+++ data/socre.ts
@@ -1,22 +1,22 @@
 export const scoreOnSubjectTitlesDefault = [
   {
-    icon: "https://storage.tatugaschool.com/AVATAR/Good-job.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Good-job.webp",
     blurHash: "UEO{GV?D05-m~9WDIqah0NWV08M~X_ows.ov",
   },
   {
-    icon: "https://storage.tatugaschool.com/AVATAR/Well-Done.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Well-Done.webp",
     blurHash: "UlMi|;xpE4n+IrWDs.bFIqahE5bY~QovIrjI",
   },
   {
-    icon: "https://storage.tatugaschool.com/AVATAR/Keep-It-Up.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Keep-It-Up.webp",
     blurHash: "UAPPF5^z05?W~RRlNIoe05WC07IY~QxrD-WD",
   },
   {
-    icon: "https://storage.tatugaschool.com/AVATAR/Excellent.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Excellent.webp",
     blurHash: "UAP63q^z06?C^}WCM~a#05WC07Ir~jt5E4oe",
   },
   {
-    icon: "https://storage.tatugaschool.com/AVATAR/Needs-Improvement.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Needs-Improvement.webp",
     blurHash: "UAPPF5^z05?W~RRlNIoe05WC07IY~QxrD-WD",
   },
 ] as const;
