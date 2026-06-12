586a1a94c136072d86ffe58eb418291eb4c87a2b
Permlap Phola <permlap@tatugacamp.com>
2026-06-12 11:39:12 +0700

    update scoreOnSubject picture

--- data/socre.ts
+++ data/socre.ts
@@ -1,22 +1,22 @@
 export const scoreOnSubjectTitlesDefault = [
   {
-    icon: "https://storage.cloud.google.com/public-tatugaschool/Good-job.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Good-job.svg",
     blurHash: "UEO{GV?D05-m~9WDIqah0NWV08M~X_ows.ov",
   },
   {
-    icon: "https://storage.cloud.google.com/public-tatugaschool/Well-Done.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Well-Done.svg",
     blurHash: "UlMi|;xpE4n+IrWDs.bFIqahE5bY~QovIrjI",
   },
   {
-    icon: "https://storage.cloud.google.com/public-tatugaschool/Keep-It-Up.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Keep-It-Up.svg",
     blurHash: "UAPPF5^z05?W~RRlNIoe05WC07IY~QxrD-WD",
   },
   {
-    icon: "https://storage.cloud.google.com/public-tatugaschool/Excellent.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Excellent.svg",
     blurHash: "UAP63q^z06?C^}WCM~a#05WC07Ir~jt5E4oe",
   },
   {
-    icon: "https://storage.cloud.google.com/public-tatugaschool/Needs-Improvement.svg",
+    icon: "https://storage.tatugaschool.com/AVATAR/Needs-Improvement.svg",
     blurHash: "UAPPF5^z05?W~RRlNIoe05WC07IY~QxrD-WD",
   },
 ] as const;
